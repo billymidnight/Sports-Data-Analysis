6191bc16616e788476bcb168333035af9d319c0c
billymidnight <139000708+billymidnight@users.noreply.github.com>
2023-09-13 02:43:27 +0000

    Update kmeans_clustering.py naming

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler 
 from sklearn.cluster import KMeans
 
 df = pd.read_csv('participants.csv')
